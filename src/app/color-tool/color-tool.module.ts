b43067735c0e4d07390b14e3302301588a2b44be
Eric W. Greene <eric@ericwgreene.com>
2018-03-19 13:45:02 -0500

    added code demos

--- src/app/color-tool/color-tool.module.ts
+++ src/app/color-tool/color-tool.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ColorHomeComponent } from './components/color-home/color-home.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, FormsModule,
   ],
   declarations: [ColorHomeComponent],
   exports: [ColorHomeComponent],
--- src/app/color-tool/components/color-home/color-home.component.ts
+++ src/app/color-tool/components/color-home/color-home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Color } from '../../models/color';
+
 @Component({
   selector: 'color-home',
   templateUrl: './color-home.component.html',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorHomeComponent implements OnInit {
 
+  colors: Color[] = [
+    { id: 1, name: 'hot pink', hexCode: '' },
+    { id: 2, name: 'blue', hexCode: '' },
+    { id: 3, name: 'green', hexCode: '' },
+    { id: 4, name: 'white', hexCode: '' },
+    { id: 5, name: 'red', hexCode: '' },
+  ];
+
+  newColorName = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addColor() {
+    this.colors.push({
+      id: Math.max(...this.colors.map(c => c.id)) + 1,
+      name: this.newColorName,
+      hexCode: '',
+    });
   }
 
 }
